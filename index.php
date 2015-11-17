5c1eaa4a4d2e62b12e4f70db391902fd325fb6c7
rcodonnell <rcoreyodonnell@gmail.com>
2015-11-16 20:40:30 -0500

    Set up form to adjust settings of widget

--- index.php
+++ index.php
@@ -19,9 +19,37 @@ class MonarchPress_Twitter_Widget extends WP_Widget {
         parent::__construct('MonarchPress_Twitter_Widget', '', $options);
     }
 
-    public function form()
+    public function form($instance)
     {
+        extract($instance);
+        ?>
+        <p>
+            <label for="<? echo $this->get_field_id('title');?>">Title: </label>
+            <input type="text" class="widefat"
+                   id="<? echo $this->get_field_id('title');?>"
+                   name="<? echo $this->get_field_name('title')?>"
+                   value="<? if(isset($title)) echo esc_attr($title);?>">
+        </p>
 
+        <p>
+            <label for="<? echo $this->get_field_id('username');?>">Twitter Username: </label>
+            <input type="text" class="widefat"
+                   id="<? echo $this->get_field_id('username');?>"
+                   name="<? echo $this->get_field_name('username')?>"
+                   value="<? if(isset($username)) echo esc_attr($username);?>">
+        </p>
+
+        <p>
+            <label for="<? echo $this->get_field_id('tweet_count');?>">Number of Tweets to Retrieve: </label>
+            <input type="number" class="widefat" style="width: 50px;"
+                   id="<? echo $this->get_field_id('tweet_count');?>"
+                   name="<? echo $this->get_field_name('tweet_count')?>"
+                   min="1"
+                   max="10"
+                   value="<? echo !empty($tweet_count) ? $tweet_count : 5; ?>">
+        </p>
+
+        <?php
     }
 
     public function widget()
