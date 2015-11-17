2294f55967c884100fc65c2d0d8ab1681b55b709
rcodonnell <rcoreyodonnell@gmail.com>
2015-11-16 21:49:55 -0500

    Setting up twitter

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ Version: 0.1
 Author URI: http://www.cs.odu.edu/~411green/
 */
 
+define( 'MONARCHPRESS__PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'MONARCHPRESS__PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 
+require_once( MONARCHPRESS__PLUGIN_DIR . 'class.monarchpress-twitter-widget.php' );
 
 ?>
