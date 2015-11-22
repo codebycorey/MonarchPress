6ca4900fa06f1fe39838e58fea6b706097a77602
Erik Wichern <ewichern@gmail.com>
2015-11-22 06:46:07 -0500

    removed unnecessary lines.

--- addMenu.php
+++ addMenu.php
@@ -10,16 +10,4 @@ function register_menu_page() {
    add_menu_page ( $page_title, $menu_title, $capability, $menu_slug, '', '', $position );
 }
 
-function my_plugin_menu() {
-   add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
-}
-
-function my_plugin_options() {
-   if ( !current_user_can( 'manage_options' ) )  {
-      wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-   }
-   echo '<div class="wrap">';
-   echo '<p>Here is where the form would go if I actually had options.</p>';
-   echo '</div>';
-}
 ?>
